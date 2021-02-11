4b44119efaa70af1d9eb9c8235b9ca4b9c96e448
VinayTS <vinay@succorfish.com>
2021-02-11 17:03:33 +0530

    Showing the Pop Up dialog Fragment is completed. UI handling and Test cases needs to be handled.

--- Code/app/src/main/java/com/succorfish/geofence/DialogFragmentHelper/ShowSOSDialogFragment.java
+++ Code/app/src/main/java/com/succorfish/geofence/DialogFragmentHelper/ShowSOSDialogFragment.java
@@ -2,32 +2,45 @@ package com.succorfish.geofence.DialogFragmentHelper;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.succorfish.geofence.R;
 import com.succorfish.geofence.adapter.SOSdialogFragmentAdapter;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class ShowSOSDialogFragment extends DialogFragment {
     SOSdialogFragmentAdapter soSdialogFragmentAdapter;
     private FragmentActivity fragmentActivity;
     private Unbinder unbinder;
-    public ShowSOSDialogFragment(){
+    private ArrayList<String> sosMessageList=new ArrayList<String>();
+    @BindView(R.id.popup_sent_msg_recyclerview_msg)
+    RecyclerView sosMessageListRecycleView;
+    public ShowSOSDialogFragment() {
 
     }
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
+        fragmentActivity = getActivity();
     }
 
     @NonNull
@@ -39,7 +52,29 @@ public class ShowSOSDialogFragment extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_sospopup, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        if (getDialog() != null && getDialog().getWindow() != null) {
+            getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        }
+        add_SOSMessages();
+        setUpRecycleView();
+        return view;
+    }
+
+    private void setUpRecycleView() {
+        soSdialogFragmentAdapter=new SOSdialogFragmentAdapter(sosMessageList);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        sosMessageListRecycleView.setLayoutManager(mLayoutManager);
+        sosMessageListRecycleView.setAdapter(soSdialogFragmentAdapter);
+    }
+
+    private void add_SOSMessages(){
+        sosMessageList.add("SOS MESSAGES 1");
+        sosMessageList.add("SOS MESSAGES 2");
+        sosMessageList.add("SOS MESSAGES 3");
+        sosMessageList.add("SOS MESSAGES 4");
     }
 
     @Override
